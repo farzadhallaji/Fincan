bbc956a1e294a4fab10343ebba040810369d840e
farzadhallaji <farzadhallaji@gmail.com>
2023-03-23 16:11:09 +0330

    adding update data

--- main.py
+++ main.py
@@ -1,45 +1,3 @@
-#from flask import Flask, render_template, request, redirect, url_for
-
-
-#import ccxt
-
-#app = Flask(__name__)
-
-#@app.route('/')
-#@app.route('/home')
-#@app.route('/index')
-#def index():
-    #return render_template('index.html')
-
-
-
-#@app.route('/')
-#def index():
-    #Get a list of all available exchanges
-    #exchanges = ccxt.exchanges
-
-    #Render the exchanges list in a template
-    #return render_template('index.html', exchanges=exchanges)
-
-
-#@app.route('/config', methods=['GET', 'POST'])
-#def config():
-    #if request.method == 'POST':
-        #api_key = request.form['api_key']
-        #api_secret = request.form['api_secret']
-        
-        #Your code to use the API key and secret
-        #...
-
-        #return redirect(url_for('index'))
-    #else:
-        #return render_template('config.html')
-
-
-
-#if __name__ == '__main__':
-    #app.run()
-
 
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -71,10 +29,11 @@ def config():
 
     return render_template('config.html', exchanges=exchanges)
 
-@app.route('/update')
-def update():
-    #dh.update_data()
-    return redirect(url_for('index'))
+@app.route('/update_data', methods=['GET', 'POST'])
+def update_data():
+    if request.method == 'GET':
+        return render_template('update_data.html', exchanges=exchanges)
+
 
 @app.route('/stimulate')
 def stimulate():
